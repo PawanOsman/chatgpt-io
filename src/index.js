e9520807a18bb2cf4a68aa8e0b53892f53a75303
Pawan Osman <contact@pawan.krd>
2022-12-29 18:57:43 +0300

    Reformat

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const uuid = require('uuid');
 const io = require('socket.io-client');
 
 class ChatGPT {
-	constructor(sessionToken, bypassNode = "https://gpt.pawan.krd") {
+    constructor(sessionToken, bypassNode = "https://gpt.pawan.krd") {
         this.ready = false;
         this.socket = io.connect(bypassNode, {
             pingInterval: 10000,
@@ -16,36 +16,36 @@ class ChatGPT {
             upgrade: false,
             forceNew: false
         });
-		this.sessionToken = sessionToken;
+        this.sessionToken = sessionToken;
         this.conversations = [];
-		this.auth = null;
-		this.expires = new Date();
-		this.pauseTokenChecks = false;
+        this.auth = null;
+        this.expires = new Date();
+        this.pauseTokenChecks = false;
         this.socket.on('connect', () => {
-          console.log('Connected to server');
+            console.log('Connected to server');
         });
         this.socket.on('disconnect', () => {
-          console.log('Disconnected from server');
+            console.log('Disconnected from server');
         });
-		setInterval(async ()=>{
-			if(this.pauseTokenChecks) return;
-			this.pauseTokenChecks = true;
-			const now = new Date();
-			const offset = 2 * 60 * 1000;
-			if (this.expires < (now - offset) || !this.auth) {
-			  await this.getTokens();
-			}
-			this.pauseTokenChecks = false;
-		}, 500);
+        setInterval(async () => {
+            if (this.pauseTokenChecks) return;
+            this.pauseTokenChecks = true;
+            const now = new Date();
+            const offset = 2 * 60 * 1000;
+            if (this.expires < (now - offset) || !this.auth) {
+                await this.getTokens();
+            }
+            this.pauseTokenChecks = false;
+        }, 500);
         setInterval(() => {
             const now = Date.now();
             this.conversations = this.conversations.filter(conversation => {
-              return now - conversation.lastActive < 1800000; // 2 minutes in milliseconds
+                return now - conversation.lastActive < 1800000; // 2 minutes in milliseconds
             });
         }, 60000);
     }
 
-    addConversation(id){
+    addConversation(id) {
         let conversation = {
             id: id,
             conversationId: null,
@@ -56,26 +56,30 @@ class ChatGPT {
         return conversation;
     }
 
-    getConversationById(id){
+    getConversationById(id) {
         let conversation = this.conversations.find(conversation => conversation.id === id);
         if (!conversation) {
-          conversation = this.addConversation(id)
+            conversation = this.addConversation(id)
         } else {
-          conversation.lastActive = Date.now();
+            conversation.lastActive = Date.now();
         }
         return conversation;
     }
 
-	async Wait(time) { return new Promise(resolve => { setTimeout(resolve, time) }) }
+    async Wait(time) {
+        return new Promise(resolve => {
+            setTimeout(resolve, time)
+        })
+    }
 
-	async waitForReady() {
-		while(!this.ready) await this.Wait(25);
-		console.log("Ready!!");
-	}
+    async waitForReady() {
+        while (!this.ready) await this.Wait(25);
+        console.log("Ready!!");
+    }
 
-	async ask(prompt, id = "default") {
-		if (!this.auth || !this.validateToken(this.auth))
-			await this.getTokens();
+    async ask(prompt, id = "default") {
+        if (!this.auth || !this.validateToken(this.auth))
+            await this.getTokens();
         let conversation = this.getConversationById(id)
         let data = await (new Promise(resolve => {
             this.socket.emit('askQuestion', {
@@ -88,34 +92,34 @@ class ChatGPT {
             })
         }));
 
-        if(data.error) console.log(`Error: ${data.error}`);
+        if (data.error) console.log(`Error: ${data.error}`);
 
         conversation.parentId = data.messageId;
         conversation.conversationId = data.conversationId;
 
         return data.answer;
-	}
+    }
 
-	validateToken(token) {
-		if (!token) return false;
-		const parsed = JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
+    validateToken(token) {
+        if (!token) return false;
+        const parsed = JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
 
-		return Date.now() <= parsed.exp * 1000;
-	}
+        return Date.now() <= parsed.exp * 1000;
+    }
 
-	async getTokens() {
-		await this.Wait(1000)
+    async getTokens() {
+        await this.Wait(1000)
         let data = await (new Promise(resolve => {
             this.socket.emit('getSession', this.sessionToken, data => {
                 resolve(data);
             })
         }));
-        if(data.error) console.log(`Error: ${data.error}`);
+        if (data.error) console.log(`Error: ${data.error}`);
         this.sessionToken = data.sessionToken;
         this.auth = data.auth;
         this.expires = data.expires;
         this.ready = true;
-	}
+    }
 }
 
 module.exports = ChatGPT;
